d3229de03ba79b988c9608a707812e295bbac6c4
uzbekcoder-max <65864876+uzbekcoder-max@users.noreply.github.com>
2020-05-29 23:02:21 +0500

    Fuksiya yaratish

---
+++ funksiya yaratish/untitled.py
@@ -0,0 +1,9 @@
+# pythonda funksiyalar har doim def dan boshlanadi
+def test_func():
+    for x in range(1,10):
+        print("Bu funksiya")
+
+# funksiyalar chaqirilmasa hech qachon ishlamaydi
+
+# funksiyainng chaqirilishi
+test_func()
